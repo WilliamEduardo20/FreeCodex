8f6ed0a8e0a99e76d6e2e0d33e5acb03d5c3be74
WilliamEduardo20 <williameduardominorelli@gmail.com>
2025-06-07 14:51:14 -0300

    14:50 07/06/2025
    Aprimoramento nas instruções de como desenvolver e estruturar as páginas do TCC, com exemplos(incompleto).

---
+++ Guia/base.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html> <!--Declara o tipo do documento-->
+<html lang="pt-BR"> <!--Declara o idioma da página-->
+<head> <!--Configurações da página-->
+    <meta charset="UTF-8"> <!--Define o tipo de caracteres suportados na página-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Configura a página para design responsivo-->
+    <title>FreeCodex - Soluções Incríveis para Você</title> <!--Define título da página-->
+    <meta name="description" content=""> <!--Descrição da página-->
+    <meta name="robots" content=""> <!--Como os bots devem tratar a página-->
+    <meta name="keywords" content=""> <!--Palavras chave relacionadas ao site-->
+    <link rel="canonical" href=""> <!--Define a URL principal da página-->
+    <link rel="stylesheet" href=""> <!--Conexão com CSS-->
+</head>
+<body> <!--Página / Site-->
+    <header> <!--Cabeçalho-->
+    </header>
+    <main> <!--Corpo-->
+    </main>
+    <footer> <!--Rodapé-->
+    </footer>
+    <aside> <!--Barra Lateral-->
+    </aside>
+</body>
+</html>
